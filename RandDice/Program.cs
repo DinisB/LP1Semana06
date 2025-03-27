482a84b19cda4034a7fd3450ff5c7a14a8bce3f4
DinisB <dinisbarroso.dev@gmail.com>
2025-03-27 21:37:46 +0000

    Stopwatch

---
+++ RandDice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sum = 0;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("You have to choose two arguments!");
+                return;
+            }
+            int.TryParse(args[0], out int n);
+            int.TryParse(args[1], out int s);
+
+            Random random = new Random(s);
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += random.Next(1, 7);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
