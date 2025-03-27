ad45ddbd6ddd9ffd34ef89fc508829732d65dc3c
DinisB <dinisbarroso.dev@gmail.com>
2025-03-27 22:22:39 +0000

    Parte do exercicio 10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,20 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("You have to write one argument!");
+                return;
+            }
+            int.TryParse(args[0], out int nInimigos);
+            Enemy[] inimigos = new Enemy[nInimigos];
+
+            for (int i = 0; i < nInimigos; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string nome = Console.ReadLine();
+                inimigos[i] = new Enemy(nome);
+            }
         }
     }
 }
